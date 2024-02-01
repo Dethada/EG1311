e276e7ca16f95269a83e4d26dbe6d9e414b374fa
David <david@dzhy.dev>
2024-02-01 13:15:23 +0800

    Add servo library

--- src/src.ino
+++ src/src.ino
@@ -1,10 +1,14 @@
+#include <Servo.h>
+
 #define TRIG_PIN 13
 #define ECHO_PIN 12
 #define MOTOR_PIN1 6
 #define MOTOR_PIN2 5
+#define SERVO_PIN 2
 
 const float SPEED_OF_SOUND = 0.0345; // in cm/μs
 const float STOPPING_DISTANCE = 5;   // in cm
+Servo servo;
 
 void setup() {
   pinMode(MOTOR_PIN1, OUTPUT);
@@ -13,6 +17,7 @@ void setup() {
   digitalWrite(TRIG_PIN, LOW);
   pinMode (ECHO_PIN, INPUT);
   Serial.begin(9600);
+  servo.attach(SERVO_PIN, 660, 2400);
 }
 
 void loop() {
@@ -25,6 +30,7 @@ void loop() {
   if (detected_distance < STOPPING_DISTANCE) { // stop motor
     digitalWrite(MOTOR_PIN1, LOW);
     digitalWrite(MOTOR_PIN2, LOW);
+    servo.write(180); // trigger slingshot
   } else {
     digitalWrite(MOTOR_PIN1, HIGH);
     digitalWrite(MOTOR_PIN2, HIGH);
